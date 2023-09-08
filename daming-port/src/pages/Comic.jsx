f7b563cdb86231d6e6ec023a7472c6998b50f27b
flavio ventureli <flavioventureli13@gmail.com>
2023-09-08 14:19:07 -0300

    conclusão da responsividade da navbar

--- daming-port/src/pages/Comic.jsx
+++ daming-port/src/pages/Comic.jsx
@@ -4,10 +4,12 @@ import './Default.scss'
 //components
 import Sidebar from "../components/Sidebar"
 import ComicComp from '../components/ComicComp';
+import Navbar from '../components/Navbar'
 
 const Comic = () => {
   return (
     <>
+    <Navbar/>
       <Box component='div' className='body'>
         <Sidebar/>
         <ComicComp/>
